603fdfe7cd8b0551ee2315e70ec69f8151321ceb
sumit1092 <sumitnandkar21@gmail.com>
2025-08-29 01:20:16 +0530

    toast added

--- src/app/components/ClientProvider.js
+++ src/app/components/ClientProvider.js
@@ -1,27 +1,3 @@
-// "use client";
-
-// import { Provider } from "react-redux";
-// import { store } from "../redux/store";
-// import { MantineProvider } from "@mantine/core";
-// import { Notifications } from "@mantine/notifications"; 
-
-// function ClientProvider({ children }) {
-//   return (
-//     <Provider store={store}>
-//       <MantineProvider
-//         withGlobalStyles
-//         withNormalizeCSS
-//         theme={{ colorScheme: "light" }}
-//         emotionOptions={{ key: "mantine", prepend: true }}
-//       >
-//         <Notifications position="top-right" />
-//         {children}
-//       </MantineProvider>
-//     </Provider>
-//   );
-// }
-
-// export default ClientProvider;
 "use client";
 
 import { Provider } from "react-redux";
